a5c8b824e2579ec6268c54344dd9e4c8e422a164
malikizaki94 <malikizakitop@gmail.com>
2024-05-22 19:21:48 +0700

    fc

---
+++ pertemuan13/index.js
@@ -0,0 +1,38 @@
+var name = "hello"
+var num = 200
+var sum = 2000 + 24 
+console.log(name);
+console.log(num);
+console.log(sum);
+
+// let lingkaran = 20
+// lingkaran = 40
+// console.log(lingkaran);
+
+// const pi = 3.14
+// pi = 10
+// console.log(pi);
+
+var hobies = ["membaca", "menulis","ngoding"]
+// var hobies 
+
+hobies[1] = "berenang"
+
+// 0,1,2, dst
+
+console.log(hobies);
+console.log(hobies[0]);
+hobies.push("berenang")
+console.log(hobies);
+hobies.pop()
+console.log(hobies);
+
+
+hitungPersegi(10,5)
+hitungPersegi(100,50)
+
+function hitungPersegi(panjang, lebar){
+    return console.log(panjang * lebar);
+}
+
+
